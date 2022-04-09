e925ee90aa42d8a7fbb8257ddc13c11eb6e981e3
Callum Irving <callum.irving@protonmail.com>
2022-04-09 00:13:16 -0400

    add panning and zooming

--- src/main.rs
+++ src/main.rs
@@ -1,93 +1,91 @@
 #![feature(portable_simd)]
-use std::time::{Duration, Instant};
-use std::{simd::*, thread};
+use std::simd::*;
 
-use minifb::{Window, WindowOptions};
+use minifb::{Key, Window, WindowOptions};
+use minifb::{MouseButton, MouseMode};
 
 const WIDTH: usize = 1280;
 const HEIGHT: usize = 720;
-const MAX_ITERS: i64 = 500;
+const MAX_ITERS: i64 = 200;
 
 fn main() {
     let mut buf = vec![0; WIDTH * HEIGHT];
-    let scale = 1000.0;
-    let x_off = -0.750222;
-    let y_off = -0.031161;
-    let mut colours = [0; MAX_ITERS as usize + 1];
-    colours[MAX_ITERS as usize] = 0x00FFFFFF;
+    let mut scale = 800.0;
+    let mut x_off = -0.750222;
+    let mut y_off = -0.031161;
 
-    let start = Instant::now();
-    draw_mandelbrot(&mut buf, scale, x_off, y_off, colours);
-    let elapsed = start.elapsed();
-    println!("Finished in {} milliseconds.", elapsed.as_millis());
+    // For panning
+    let mut prev_mouse_pos: Option<(f32, f32)> = None;
 
-    let start = Instant::now();
-    draw_mandelbrot_vectorized(&mut buf, scale, x_off, y_off, colours);
-    let elapsed = start.elapsed();
-    println!("Finished in {} milliseconds.", elapsed.as_millis());
+    // Precompute colours
+    let colours = precompute_complex(MAX_ITERS as usize);
 
+    // Create window
     let mut window =
         Window::new("Mandelbrot Set", WIDTH, HEIGHT, WindowOptions::default()).unwrap();
-    window.update_with_buffer(&buf, WIDTH, HEIGHT).unwrap();
 
-    thread::sleep(Duration::from_millis(2000));
+    // Event loop
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        draw_mandelbrot_vectorized(&mut buf, scale, x_off, y_off, &colours);
+        window.update_with_buffer(&buf, WIDTH, HEIGHT).unwrap();
+
+        // Panning
+        if window.get_mouse_down(MouseButton::Left) {
+            let current_pos = window.get_mouse_pos(MouseMode::Pass).unwrap();
+            if let Some(prev_mouse_pos) = prev_mouse_pos {
+                x_off += (prev_mouse_pos.0 - current_pos.0) as f64 / scale;
+                y_off += (prev_mouse_pos.1 - current_pos.1) as f64 / scale;
+            }
+            prev_mouse_pos = Some(current_pos);
+        } else {
+            prev_mouse_pos = None;
+        }
+
+        // Zooming
+        if let Some(scroll) = window.get_scroll_wheel() {
+            let mouse_pos = window.get_mouse_pos(MouseMode::Pass).unwrap();
+            let prev_x = mouse_pos.0 as f64 / scale + x_off;
+            let prev_y = mouse_pos.1 as f64 / scale + y_off;
+            if scroll.1 > 0.0 {
+                scale *= 1.1;
+            } else {
+                scale /= 1.1;
+            }
+            let curr_x = mouse_pos.0 as f64 / scale + x_off;
+            let curr_y = mouse_pos.1 as f64 / scale + y_off;
+            x_off += prev_x - curr_x;
+            y_off += prev_y - curr_y;
+        }
+    }
 }
 
+#[inline]
 fn from_rgb(r: u8, g: u8, b: u8) -> u32 {
     let (r, g, b) = (r as u32, g as u32, b as u32);
     r << 16 | g << 8 | b
 }
 
-fn draw_mandelbrot(
-    buffer: &mut [u32],
-    scale: f64,
-    x_off: f64,
-    y_off: f64,
-    colours: [u32; MAX_ITERS as usize + 1],
-) {
-    let mut iters = [0; WIDTH * HEIGHT];
-
-    for y in 0..HEIGHT {
-        let ci = y as f64 / scale + y_off;
-        for x in 0..WIDTH {
-            let cr = x as f64 / scale + x_off;
-
-            let mut zr = 0.0;
-            let mut zi = 0.0;
-
-            let mut n: usize = 0;
-
-            for _ in 0..MAX_ITERS {
-                let new_zr = zr * zr - zi * zi + cr;
-                let new_zi = zr * zi * 2.0 + ci;
-                zr = new_zr;
-                zi = new_zi;
-
-                let squared_abs = zr * zr + zi * zi;
-                if squared_abs >= 4.0 {
-                    break;
-                }
-                n += 1;
-            }
-
-            iters[y * WIDTH + x] = n;
-        }
+fn precompute_complex(iters: usize) -> Vec<u32> {
+    let mut colours = vec![0; iters + 1];
+    let a = 0.1;
+    for (i, colour) in colours.iter_mut().enumerate() {
+        let r = (0.5 * (a * i as f32).sin() + 0.5) * 255.0;
+        let g = (0.5 * (a * i as f32 + 2.094).sin() + 0.5) * 255.0;
+        let b = (0.5 * (a * i as f32 + 4.188).sin() + 0.5) * 255.0;
+        *colour = from_rgb(r as u8, g as u8, b as u8);
     }
-
-    for (i, pixel) in buffer.iter_mut().enumerate() {
-        *pixel = colours[iters[i]];
-    }
+    colours
 }
 
+const TWO: f64x4 = f64x4::splat(2.0);
+const FOUR: f64x4 = f64x4::splat(4.0);
 fn draw_mandelbrot_vectorized(
     buffer: &mut [u32],
     scale: f64,
     x_off: f64,
     y_off: f64,
-    colours: [u32; MAX_ITERS as usize + 1],
+    colours: &[u32],
 ) {
-    let mut iters = [0; WIDTH * HEIGHT];
-
     let scale_vec = f64x4::splat(scale);
     let x_off_vec = f64x4::splat(x_off);
 
@@ -105,7 +103,7 @@ fn draw_mandelbrot_vectorized(
             for _ in 0..MAX_ITERS {
                 // Square z and add c
                 let new_zr = zr * zr - zi * zi + cr;
-                let new_zi = f64x4::splat(2.0) * zr * zi + ci;
+                let new_zi = zr * zi * TWO + ci;
                 zr = new_zr;
                 zi = new_zi;
 
@@ -113,7 +111,7 @@ fn draw_mandelbrot_vectorized(
                 let squared_abs = zr * zr + zi * zi;
 
                 // True means we need to increment
-                let not_diverged = squared_abs.lanes_lt(f64x4::splat(4.0));
+                let not_diverged = squared_abs.lanes_lt(FOUR);
 
                 // Increment n by one
                 let to_add = i64x4::splat(1) & not_diverged.to_int();
@@ -126,15 +124,11 @@ fn draw_mandelbrot_vectorized(
                 }
             }
 
-            // Save calculated values in iters array
-            iters[y * WIDTH + x] = n[0] as usize;
-            iters[y * WIDTH + x + 1] = n[1] as usize;
-            iters[y * WIDTH + x + 2] = n[2] as usize;
-            iters[y * WIDTH + x + 3] = n[3] as usize;
+            // Save calculated values in buffer as colours
+            buffer[y * WIDTH + x] = colours[n[0] as usize];
+            buffer[y * WIDTH + x + 1] = colours[n[1] as usize];
+            buffer[y * WIDTH + x + 2] = colours[n[2] as usize];
+            buffer[y * WIDTH + x + 3] = colours[n[3] as usize];
         }
-    }
-
-    for (i, pixel) in buffer.iter_mut().enumerate() {
-        *pixel = colours[iters[i]];
     }
 }
