a715ab5ba55da9072967f5cd61ed9a12988925c8
Callum Irving <callum.irving@protonmail.com>
2022-04-09 16:44:03 -0400

    switch back to pixels instead of minifb

--- src/main.rs
+++ src/main.rs
@@ -2,106 +2,146 @@
 use std::cmp::max;
 use std::simd::*;
 
-use minifb::{Key, KeyRepeat, Window, WindowOptions};
-use minifb::{MouseButton, MouseMode};
+use pixels::{Pixels, SurfaceTexture};
+use winit::{
+    dpi::LogicalSize,
+    event::{Event, VirtualKeyCode},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+use winit_input_helper::WinitInputHelper;
 
 const WIDTH: usize = 1280;
 const HEIGHT: usize = 720;
 
 fn main() {
     let mut max_iters: usize = 200;
-    let mut buf = vec![0; WIDTH * HEIGHT];
     let mut scale = 800.0;
     let mut x_off = -0.750222;
     let mut y_off = -0.031161;
 
-    // For panning
-    let mut prev_mouse_pos: Option<(f32, f32)> = None;
+    // Window stuff
+    let event_loop = EventLoop::new();
+    let mut input = WinitInputHelper::new();
 
-    // Precompute colours
-    let mut colours = vec![];
-    colours = precompute_complex(max_iters + 1, colours);
+    let window = {
+        let size = LogicalSize::new(WIDTH as f64, HEIGHT as f64);
+        WindowBuilder::new()
+            .with_title("Mandelbrot Set")
+            .with_inner_size(size)
+            .with_min_inner_size(size)
+            .build(&event_loop)
+            .unwrap()
+    };
 
-    // Create window
-    let mut window =
-        Window::new("Mandelbrot Set", WIDTH, HEIGHT, WindowOptions::default()).unwrap();
+    let mut pixels = {
+        let window_size = window.inner_size();
+        let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
+        Pixels::new(WIDTH as u32, HEIGHT as u32, surface_texture).unwrap()
+    };
 
-    // Event loop
-    while window.is_open() && !window.is_key_down(Key::Escape) {
-        // Draw Mandelbrot set
-        draw_mandelbrot_vectorized(&mut buf, scale, x_off, y_off, &colours, max_iters);
-        window.update_with_buffer(&buf, WIDTH, HEIGHT).unwrap();
-
-        // Panning
-        if window.get_mouse_down(MouseButton::Left) {
-            let current_pos = window.get_mouse_pos(MouseMode::Pass).unwrap();
-            if let Some(prev_mouse_pos) = prev_mouse_pos {
-                x_off += (prev_mouse_pos.0 - current_pos.0) as f64 / scale;
-                y_off += (prev_mouse_pos.1 - current_pos.1) as f64 / scale;
+    event_loop.run(move |event, _, control_flow| {
+        if let Event::RedrawRequested(_) = event {
+            draw_mandelbrot_vectorized(
+                pixels.get_frame(),
+                scale,
+                x_off,
+                y_off,
+                // &colours,
+                max_iters,
+            );
+            if pixels
+                .render()
+                .map_err(|e| eprintln!("pixels.render() failed: {}", e))
+                .is_err()
+            {
+                *control_flow = ControlFlow::Exit;
+                return;
             }
-            prev_mouse_pos = Some(current_pos);
-        } else {
-            prev_mouse_pos = None;
         }
 
-        // Zooming
-        if let Some(scroll) = window.get_scroll_wheel() {
-            let mouse_pos = window.get_mouse_pos(MouseMode::Pass).unwrap();
-            let prev_x = mouse_pos.0 as f64 / scale + x_off;
-            let prev_y = mouse_pos.1 as f64 / scale + y_off;
-            if scroll.1 > 0.0 {
-                scale *= 1.1;
-            } else {
-                scale /= 1.1;
+        if input.update(&event) {
+            if input.key_pressed(VirtualKeyCode::Escape) || input.quit() {
+                *control_flow = ControlFlow::Exit;
+                return;
             }
-            let curr_x = mouse_pos.0 as f64 / scale + x_off;
-            let curr_y = mouse_pos.1 as f64 / scale + y_off;
-            x_off += prev_x - curr_x;
-            y_off += prev_y - curr_y;
+
+            // Get mouse position info
+            let (mouse_cell, mouse_prev_cell) = input
+                .mouse()
+                .map(|(mx, my)| {
+                    let (dx, dy) = input.mouse_diff();
+                    let prev_x = mx - dx;
+                    let prev_y = my - dy;
+
+                    let (mx_i, my_i) = pixels
+                        .window_pos_to_pixel((mx, my))
+                        .unwrap_or_else(|pos| pixels.clamp_pixel_pos(pos));
+
+                    let (px_i, py_i) = pixels
+                        .window_pos_to_pixel((prev_x, prev_y))
+                        .unwrap_or_else(|pos| pixels.clamp_pixel_pos(pos));
+
+                    (
+                        (mx_i as isize, my_i as isize),
+                        (px_i as isize, py_i as isize),
+                    )
+                })
+                .unwrap_or_default();
+
+            // Pan
+            if input.mouse_held(0) {
+                x_off -= (mouse_cell.0 - mouse_prev_cell.0) as f64 / scale;
+                y_off -= (mouse_cell.1 - mouse_prev_cell.1) as f64 / scale;
+            }
+
+            // Zoom
+            if input.scroll_diff() != 0.0 {
+                let prev_x = mouse_cell.0 as f64 / scale - x_off;
+                let prev_y = mouse_cell.1 as f64 / scale - y_off;
+
+                if input.scroll_diff() > 0.0 {
+                    scale *= 1.2 * input.scroll_diff() as f64;
+                } else {
+                    scale /= 1.2 * input.scroll_diff().abs() as f64;
+                }
+
+                let new_x = mouse_cell.0 as f64 / scale - x_off;
+                let new_y = mouse_cell.1 as f64 / scale - y_off;
+                x_off += prev_x - new_x;
+                y_off += prev_y - new_y;
+            }
+
+            // Change number of iterations
+            if input.key_pressed(VirtualKeyCode::Up) {
+                max_iters += 10;
+            }
+
+            if input.key_pressed(VirtualKeyCode::Down) {
+                max_iters = max(10, max_iters - 10);
+            }
+
+            window.request_redraw();
         }
-
-        // Changing number of iterations
-        window
-            .get_keys_pressed(KeyRepeat::Yes)
-            .iter()
-            .for_each(|key| match key {
-                Key::Up => max_iters += 10,
-                Key::Down => max_iters = max(max_iters - 10, 10),
-                _ => (),
-            });
-        colours = precompute_complex(max_iters + 1, colours);
-    }
+    });
 }
 
-#[inline]
-fn from_rgb(r: u8, g: u8, b: u8) -> u32 {
-    let (r, g, b) = (r as u32, g as u32, b as u32);
-    r << 16 | g << 8 | b
-}
-
-fn precompute_complex(iters: usize, mut colours: Vec<u32>) -> Vec<u32> {
-    let old_len = colours.len();
+fn compute_colour(n: i64) -> (u8, u8, u8) {
     let a = 0.1;
-    if iters > old_len {
-        colours.reserve(iters - old_len);
-        for i in old_len..iters {
-            let r = (0.5 * (a * i as f32).sin() + 0.5) * 255.0;
-            let g = (0.5 * (a * i as f32 + 2.094).sin() + 0.5) * 255.0;
-            let b = (0.5 * (a * i as f32 + 4.188).sin() + 0.5) * 255.0;
-            colours.push(from_rgb(r as u8, g as u8, b as u8));
-        }
-    }
-    colours
+    let r = (0.5 * (a * n as f32).sin() + 0.5) * 255.0;
+    let g = (0.5 * (a * n as f32 + 2.094).sin() + 0.5) * 255.0;
+    let b = (0.5 * (a * n as f32 + 4.188).sin() + 0.5) * 255.0;
+    (r as u8, g as u8, b as u8)
 }
 
 const TWO: f64x4 = f64x4::splat(2.0);
 const FOUR: f64x4 = f64x4::splat(4.0);
 fn draw_mandelbrot_vectorized(
-    buffer: &mut [u32],
+    buffer: &mut [u8],
     scale: f64,
     x_off: f64,
     y_off: f64,
-    colours: &[u32],
+    // colours: &[u32],
     max_iters: usize,
 ) {
     let scale_vec = f64x4::splat(scale);
@@ -143,10 +183,29 @@ fn draw_mandelbrot_vectorized(
             }
 
             // Save calculated values in buffer as colours
-            buffer[y * WIDTH + x] = colours[n[0] as usize];
-            buffer[y * WIDTH + x + 1] = colours[n[1] as usize];
-            buffer[y * WIDTH + x + 2] = colours[n[2] as usize];
-            buffer[y * WIDTH + x + 3] = colours[n[3] as usize];
+            let (r, g, b) = compute_colour(n[0]);
+            buffer[(y * WIDTH + x) * 4] = r;
+            buffer[(y * WIDTH + x) * 4 + 1] = g;
+            buffer[(y * WIDTH + x) * 4 + 2] = b;
+            buffer[(y * WIDTH + x) * 4 + 3] = 255;
+
+            let (r, g, b) = compute_colour(n[1]);
+            buffer[(y * WIDTH + x + 1) * 4] = r;
+            buffer[(y * WIDTH + x + 1) * 4 + 1] = g;
+            buffer[(y * WIDTH + x + 1) * 4 + 2] = b;
+            buffer[(y * WIDTH + x + 1) * 4 + 3] = 255;
+
+            let (r, g, b) = compute_colour(n[2]);
+            buffer[(y * WIDTH + x + 2) * 4] = r;
+            buffer[(y * WIDTH + x + 2) * 4 + 1] = g;
+            buffer[(y * WIDTH + x + 2) * 4 + 2] = b;
+            buffer[(y * WIDTH + x + 2) * 4 + 3] = 255;
+
+            let (r, g, b) = compute_colour(n[3]);
+            buffer[(y * WIDTH + x + 3) * 4] = r;
+            buffer[(y * WIDTH + x + 3) * 4 + 1] = g;
+            buffer[(y * WIDTH + x + 3) * 4 + 2] = b;
+            buffer[(y * WIDTH + x + 3) * 4 + 3] = 255;
         }
     }
 }
