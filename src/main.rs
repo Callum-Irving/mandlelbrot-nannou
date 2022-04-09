e9c7fc1249b53f2a03a1c29df8d5a317b90c97c4
Callum Irving <callum.irving@protonmail.com>
2022-04-09 12:13:54 -0400

    add ability to change number of iterations

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 #![feature(portable_simd)]
+use std::cmp::max;
 use std::simd::*;
 
-use minifb::{Key, Window, WindowOptions};
+use minifb::{Key, KeyRepeat, Window, WindowOptions};
 use minifb::{MouseButton, MouseMode};
 
 const WIDTH: usize = 1280;
 const HEIGHT: usize = 720;
-const MAX_ITERS: i64 = 200;
 
 fn main() {
+    let mut max_iters: usize = 200;
     let mut buf = vec![0; WIDTH * HEIGHT];
     let mut scale = 800.0;
     let mut x_off = -0.750222;
@@ -18,7 +19,8 @@ fn main() {
     let mut prev_mouse_pos: Option<(f32, f32)> = None;
 
     // Precompute colours
-    let colours = precompute_complex(MAX_ITERS as usize);
+    let mut colours = vec![];
+    colours = precompute_complex(max_iters + 1, colours);
 
     // Create window
     let mut window =
@@ -26,7 +28,8 @@ fn main() {
 
     // Event loop
     while window.is_open() && !window.is_key_down(Key::Escape) {
-        draw_mandelbrot_vectorized(&mut buf, scale, x_off, y_off, &colours);
+        // Draw Mandelbrot set
+        draw_mandelbrot_vectorized(&mut buf, scale, x_off, y_off, &colours, max_iters);
         window.update_with_buffer(&buf, WIDTH, HEIGHT).unwrap();
 
         // Panning
@@ -56,6 +59,17 @@ fn main() {
             x_off += prev_x - curr_x;
             y_off += prev_y - curr_y;
         }
+
+        // Changing number of iterations
+        window
+            .get_keys_pressed(KeyRepeat::Yes)
+            .iter()
+            .for_each(|key| match key {
+                Key::Up => max_iters += 10,
+                Key::Down => max_iters = max(max_iters - 10, 10),
+                _ => (),
+            });
+        colours = precompute_complex(max_iters + 1, colours);
     }
 }
 
@@ -65,14 +79,17 @@ fn from_rgb(r: u8, g: u8, b: u8) -> u32 {
     r << 16 | g << 8 | b
 }
 
-fn precompute_complex(iters: usize) -> Vec<u32> {
-    let mut colours = vec![0; iters + 1];
+fn precompute_complex(iters: usize, mut colours: Vec<u32>) -> Vec<u32> {
+    let old_len = colours.len();
     let a = 0.1;
-    for (i, colour) in colours.iter_mut().enumerate() {
-        let r = (0.5 * (a * i as f32).sin() + 0.5) * 255.0;
-        let g = (0.5 * (a * i as f32 + 2.094).sin() + 0.5) * 255.0;
-        let b = (0.5 * (a * i as f32 + 4.188).sin() + 0.5) * 255.0;
-        *colour = from_rgb(r as u8, g as u8, b as u8);
+    if iters > old_len {
+        colours.reserve(iters - old_len);
+        for i in old_len..iters {
+            let r = (0.5 * (a * i as f32).sin() + 0.5) * 255.0;
+            let g = (0.5 * (a * i as f32 + 2.094).sin() + 0.5) * 255.0;
+            let b = (0.5 * (a * i as f32 + 4.188).sin() + 0.5) * 255.0;
+            colours.push(from_rgb(r as u8, g as u8, b as u8));
+        }
     }
     colours
 }
@@ -85,6 +102,7 @@ fn draw_mandelbrot_vectorized(
     x_off: f64,
     y_off: f64,
     colours: &[u32],
+    max_iters: usize,
 ) {
     let scale_vec = f64x4::splat(scale);
     let x_off_vec = f64x4::splat(x_off);
@@ -100,7 +118,7 @@ fn draw_mandelbrot_vectorized(
             let mut zr = f64x4::splat(0.0);
             let mut zi = f64x4::splat(0.0);
 
-            for _ in 0..MAX_ITERS {
+            for _ in 0..max_iters {
                 // Square z and add c
                 let new_zr = zr * zr - zi * zi + cr;
                 let new_zi = zr * zi * TWO + ci;
